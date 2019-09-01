7ae84a30cba67eb6834cc3006fa0d3ca53239db5
Ahmad Hamed Rangeen <rangeen.ahmad@gmail.com>
2019-09-01 15:08:31 -0700

    Handling Form submission

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,12 +3,22 @@ import React from "react";
 class SearchBar extends React.Component {
   state = { term: "" };
 
-  onInputChange = () => {};
+  //when you type something in search, it will update here
+  onInputChange = event => {
+    this.setState({ term: event.target.value });
+  };
+
+  onFormSubmit = event => {
+    // this is so page doesnt refresh when u push enter
+    event.preventDefault();
+
+    //Todo: make sure we call callback from parent component
+  };
 
   render() {
     return (
       <div className="search-bar ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label>Video Search</label>
             <input
