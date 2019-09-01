71578c3826fafd7295d75765af4742c43d9c3b68
Ahmad Hamed Rangeen <rangeen.ahmad@gmail.com>
2019-09-01 14:57:57 -0700

    SearchBar and semantic ui added

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,13 @@ import SearchBar from "./SearchBar";
 
 class App extends React.Component {
   render() {
-    return <SearchBar />;
+    return (
+      //ui container puts a little bit of margin
+      //on left and right side of of searchbar
+      <div className="ui container">
+        <SearchBar />
+      </div>
+    );
   }
 }
 
