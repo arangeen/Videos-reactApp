6565048429ca512a5c22980d2db4c047fdc8c19a
Ahmad Hamed Rangeen <rangeen.ahmad@gmail.com>
2019-09-03 15:11:06 -0700

    Installed axios and made http request to youtube api

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,13 @@
+import axios from "axios";
+
 const KEY = "AIzaSyBo00y1YAv6RazQixk1IRi9WF3xmmUPhGI";
+// npm install axios@0.18.1
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    maxResults: 5,
+    key: KEY
+  }
+});
